ceb419dbbcae96e6b3485fff748ef6e894ebaf06
silvina <silbordon.89@gmail.com>
2022-07-28 10:09:23 -0300

    create/adde challenche

---
+++ app.js
@@ -0,0 +1,62 @@
+// Desafio clase 4 crea un algoritmo utilizando un ciclo
+
+//Ejemplo 1
+// let usuarioName = prompt('ingrese su nombre');
+// let numero = Number(prompt('ingrese el numero (1 al 9) de la tabla que desea visualizar'));
+
+// if(Number.isInteger(numero) && numero!== 0) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(usuarioName + " eligio ver la tabla del " + numero + " y es la siguiente: " + numero + "x" + i + " = " + numero * i);
+//       }
+
+// } else{
+//     alert("El valor ingresado no es valido")
+// }
+
+
+/* ********************************************************************* */
+//Ejemplo 2
+// let palabra = "";
+// let texto = "";
+
+// do {
+//   texto = prompt("Ingrese una palabra, si ingresa ESC se termina el programa");
+//   palabra = palabra + " " + texto;
+//   alert(palabra);
+// } while (texto !== "ESC");
+
+
+/* ********************************************************************* */
+//Ejemplo 3
+
+/* let operador;
+let num;
+let num2;
+
+do {
+  operador = prompt(
+    "Ingrese su operador + , -, /, * ,para finalizar escriba FIN"
+  );
+  if (operador !== "FIN") {
+    num = Number(prompt("Ingrese el primer numero para su calculo"));
+    num2 = Number(prompt("Ingrese el segundo numero para su calculo"));
+    if (operador === "+") {
+      alert(num2 + num);
+    }
+    if (operador === "-") {
+      alert(num2 - num);
+    }
+    if (operador === "/") {
+      alert(num2 / num);
+    }
+    if (operador === "*") {
+      alert(num2 * num);
+    }
+  } else {
+    break;
+  }
+} while (operador !== "FIN");
+{
+  alert("Fin del proceso");
+}
+ */
